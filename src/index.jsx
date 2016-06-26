56f72a5c2b27997bf356ff6e6d4e32de3a8bdefc
Sergiu Buciuc <buciuc_sergiu@yahoo.com>
2016-06-26 20:36:09 +0300

    fix eslint errors

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ let markers = [];
 function clearMarkers() {
   markers.forEach(marker => {
     google.maps.event.clearInstanceListeners(marker);
-    marker.setMap(null)
+    marker.setMap(null);
   });
   markers = [];
 }
@@ -23,19 +23,19 @@ const markerIcon = {
   fillColor: 'red',
   fillOpacity: 1,
   anchor: new google.maps.Point(22, -12),
-}
+};
 
 class GoogleMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       markerLocations: [],
-      map: null
+      map: null,
     };
   }
-  componentDidMount(rootNode) {
-    const {center, zoom} = this.props;
-    const map = new google.maps.Map(this._mapEl, {center, zoom});
+  componentDidMount() {
+    const { center, zoom } = this.props;
+    const map = new google.maps.Map(this.mapEl, { center, zoom });
     map.addListener('click', e => {
       const markerLocations = [e.latLng, ...this.state.markerLocations];
       this.setState({ markerLocations });
@@ -44,16 +44,16 @@ class GoogleMap extends React.Component {
       const markerLocations = [e.latLng, ...this.state.markerLocations];
       this.setState({ markerLocations });
     });
-    this.setState({ map });
+    this.map = map;
   }
   componentDidUpdate() {
-    const { markerLocations, map } = this.state;
+    const { markerLocations } = this.state;
 
     clearMarkers();
     markers = markerLocations.map(location => {
       const marker = new google.maps.Marker({
         position: location,
-        map,
+        map: this.map,
         icon: markerIcon,
       });
       marker.addListener('dblclick', e => {
@@ -69,23 +69,28 @@ class GoogleMap extends React.Component {
   }
 
   removeMarker(marker) {
-    const { markerLocations, map } = this.state;
+    const { markerLocations } = this.state;
     const newMarkerLocations = markerLocations.filter(ml => !ml.equals(marker.getPosition()));
-    this.setState({ markerLocations : newMarkerLocations })
+    this.setState({ markerLocations: newMarkerLocations });
   }
   render() {
     return (
-      <div className='map col-lg-6' ref={mapEl => this._mapEl = mapEl}></div>
+      <div className="map col-lg-6" ref={mapEl => { this.mapEl = mapEl; }}></div>
     );
   }
 }
 
+GoogleMap.propTypes = {
+  center: React.PropTypes.object,
+  zoom: React.PropTypes.number,
+};
+
 GoogleMap.defaultProps = {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
+  center: { lat: -34.397, lng: 150.644 },
+  zoom: 8,
 };
 
 ReactDOM.render(
-  <GoogleMap></GoogleMap>,
+  <GoogleMap />,
   document.getElementById('app')
 );
