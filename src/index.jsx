e8a20ec4ae526e103746725fc952953b649c0f53
Sergiu Buciuc <buciuc_sergiu@yahoo.com>
2016-06-26 15:21:45 +0300

    markers

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,35 @@ import ReactDOM from 'react-dom';
 import './assets/stylesheets/style';
 import 'bootstrap-grid';
 
+let markers = [];
+
+function clearMarkers() {
+  markers.forEach(marker => marker.setMap(null));
+  markers = [];
+}
+
 class GoogleMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markerLocations: [],
+      map: null
+    };
+  }
   componentDidMount(rootNode) {
     const {center, zoom} = this.props;
     const map = new google.maps.Map(this._mapEl, {center, zoom});
+    map.addListener('click', e => {
+      const markerLocations = [e.latLng, ...this.state.markerLocations];
+      this.setState({ markerLocations });
+    });
+    this.setState({ map });
+  }
+  componentDidUpdate() {
+    const { markerLocations, map } = this.state;
+
+    clearMarkers();
+    markers = markerLocations.map(location => new google.maps.Marker({position: location, map }));
   }
   render() {
     return (
