e9370b68b8d7037e2373dc7ab17a837fe66519b0
Sergiu Buciuc <sbuciuc@axway.com>
2016-07-01 12:38:14 +0300

    change color on hover

--- src/components/MarkerLocation.jsx
+++ src/components/MarkerLocation.jsx
@@ -3,33 +3,45 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
-import { red500, grey500 } from 'material-ui/styles/colors';
+import { red500, grey500, yellow500 } from 'material-ui/styles/colors';
 
-export default function MarkerLocation({ id, location, onRemove }) {
-  return (
-    <div
-      className="marker-location"
-      key={id}
-    >
-      <FontIcon
-        className="drag-handle material-icons"
-        color={grey500}
+export default class MarkerLocation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false,
+    };
+  }
+  render() {
+    const { id, location, onRemove } = this.props;
+    const { hovered } = this.state;
+    return (
+      <div
+        className="marker-location"
+        key={id}
+        onMouseEnter={() => this.setState({ hovered: true })}
+        onMouseLeave={() => this.setState({ hovered: false })}
       >
-        drag_handle
-      </FontIcon>
-      <TextField
-        style={{
-          fontSize: 12,
-        }}
-        id={`tf-${id}`}
-        value={location}
-        fullWidth
-      />
-      <IconButton touch onClick={() => onRemove && onRemove(id)}>
-        <FontIcon className="material-icons" color={red500}>clear</FontIcon>
-      </IconButton>
-    </div>
-  );
+        <FontIcon
+          className="drag-handle material-icons"
+          color={hovered ? yellow500 : grey500}
+        >
+          drag_handle
+        </FontIcon>
+        <TextField
+          style={{
+            fontSize: 12,
+          }}
+          id={`tf-${id}`}
+          value={location}
+          fullWidth
+        />
+        <IconButton touch onClick={() => onRemove && onRemove(id)}>
+          <FontIcon className="material-icons" color={red500}>clear</FontIcon>
+        </IconButton>
+      </div>
+    );
+  }
 }
 
 MarkerLocation.propTypes = {
