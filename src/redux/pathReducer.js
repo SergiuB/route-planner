5bedea598ee752d3cce48bde31d93f945eeba2f4
sergiub <buciuc_sergiu@yahoo.com>
2016-07-09 23:56:45 +0300

    use destructuring instead of Object.assign

--- src/redux/pathReducer.js
+++ src/redux/pathReducer.js
@@ -14,21 +14,26 @@ export default function createPathReducer({ generateId = () => uuid.v4() } = {})
   return function path(state = initialState, action) {
     const { markers } = state;
     switch (action.type) {
+
       case types.ADD_MARKER:
         const newMarker = {
           id: generateId(),
           location: action.location,
           address: action.address,
         };
-        return Object.assign({}, state, {
+        return {
+          ...state,
           markers: _.concat(markers, [newMarker])
                     .map(m => ({ ...m })),
-        });
+        };
+
       case types.REMOVE_MARKER:
-        return Object.assign({}, state, {
+        return {
+          ...state,
           markers: _.reject(markers, ({ id }) => id === action.id)
                     .map(m => ({ ...m })),
-        });
+        };
+
       case types.UPDATE_MARKER_LOCATION:
         const markersCopy = markers.map(m => ({ ...m }));
         const marker = _.find(markersCopy, ({ id }) => id === action.id);
@@ -36,7 +41,11 @@ export default function createPathReducer({ generateId = () => uuid.v4() } = {})
           marker.location = action.location;
           marker.address = action.address;
         }
-        return Object.assign({}, state, { markers: markersCopy });
+        return {
+          ...state,
+          markers: markersCopy,
+        };
+
       default:
         return state;
     }
