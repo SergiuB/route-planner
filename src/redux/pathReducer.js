0ccc636c85d84de176ce01f3133c8d8d29381c5a
sergiub <buciuc_sergiu@yahoo.com>
2016-07-09 22:16:08 +0300

    implement ADD_MARKER

--- src/redux/pathReducer.js
+++ src/redux/pathReducer.js
@@ -1,13 +1,29 @@
-// import { ADD_TODO } from '../constants/ActionTypes'
+/* eslint no-case-declarations:0 */
+
+import uuid from 'node-uuid';
+import _ from 'lodash';
+import { ADD_MARKER } from './actionConstants';
+
 
 const initialState = {
   markers: [],
   segments: [],
 };
 
-export default function todos(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+export default function createPathReducer({ generateId = () => uuid.v4() } = {}) {
+  return function path(state = initialState, action) {
+    switch (action.type) {
+      case ADD_MARKER:
+        const { markers } = state;
+        const newMarker = {
+          id: generateId(),
+          location: action.location,
+        };
+        return Object.assign({}, state, {
+          markers: _.concat(markers, [newMarker]),
+        });
+      default:
+        return state;
+    }
+  };
 }
--- src/redux/test/pathReducer.spec.js
+++ src/redux/test/pathReducer.spec.js
@@ -1,12 +1,38 @@
 import { expect } from 'chai';
-import pathReducer from '../pathReducer';
+import createPathReducer from '../pathReducer';
 import * as actions from '../actionConstants';
 
 describe('path reducer', () => {
   it('returns the initial state', () => {
+    const pathReducer = createPathReducer();
     expect(pathReducer(undefined, {})).to.deep.equal({
       markers: [],
       segments: [],
+    });
+  });
+
+  it('handles ADD_MARKER', () => {
+    const pathReducer = createPathReducer({
+      generateId: () => 1,
+    });
+
+    expect(pathReducer(undefined, {
+      type: actions.ADD_MARKER,
+      location: [1, 1],
+    })).to.deep.equal({
+      markers: [{ id: 1, location: [1, 1] }],
+      segments: [],
+    });
+
+    expect(pathReducer({
+      markers: [{ id: 'othermarker' }],
+      segments: [1, 2, 3],
+    }, {
+      type: actions.ADD_MARKER,
+      location: [1, 1],
+    })).to.deep.equal({
+      markers: [{ id: 'othermarker' }, { id: 1, location: [1, 1] }],
+      segments: [1, 2, 3],
     });
   });
 });
